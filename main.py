355f8fe9c24c9446123e92b31159eb43b1346ad6
KaterynaTkachuk <chelovek_k@ukr.net>
2023-11-24 13:53:51 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,79 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    # реалізація класу
+    pass
+
+class Phone(Field):
+    def __init__(self, value):
+        if not self.validate(value):
+            raise ValueError("Invalid phone number")
+        super().__init__(value)
+    
+    def validate(self, value):
+        return isinstance(value, str) and len(value) == 10 and value.isdigit()
+
+    # реалізація класу
+    
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone_number: str):
+        phone = Phone(phone_number)
+        phone.validate(phone_number)
+        if phone not in self.phones:
+            self.phones.append(phone)
+
+    def find_phone(self, phone_number: str):
+        for phone in self.phones:
+            if phone.value == phone_number:
+                return phone
+        return None    
+            
+    def edit_phone (self, old_phone, new_phone):
+        count = 0
+        for phone in self.phones:
+            count += 1
+            if phone.value == old_phone:
+                next_number = Phone(new_phone)
+                next_number.validate(new_phone)
+                self.phones[self.phones.index(phone)] = next_number
+                break
+            if count == len(self.phones):
+                raise ValueError
+        
+    
+    def remove_phone (self, phone_number):
+        for phone in self.phones:
+            if phone.value == phone_number:
+                self.phones.remove(phone)
+
+    # реалізація класу
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+class AddressBook(UserDict):
+    # реалізація класу
+    def add_record(self, record: Record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        if name in self.data:
+            return self.data[name]
+        else:
+            return None
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
